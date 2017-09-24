40a6cd303f48deaa3ab4c984c865e82476764e6f
Ed Sweeney <ed@onextent.com>
2017-09-24 13:57:26 -0700

    cleanup

--- src/main/g8/src/main/scala/$package$/Main.scala
+++ src/main/g8/src/main/scala/$package$/Main.scala
@@ -1,23 +1,19 @@
 package $package$
 
-import java.util.Date
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 import com.typesafe.scalalogging.LazyLogging
 import $package$.models.{JsonSupport, Message}
 import $package$.routes.{$httpBasePath;format="Camel"$Route, $httpBasePath;format="Camel"$SegmentRoute}
-import spray.json._
 import scala.concurrent.ExecutionContextExecutor
 
 object Main extends LazyLogging with JsonSupport with ErrorSupport {
 
   def main(args: Array[String]) {
 
-    implicit val system: ActorSystem = ActorSystem("hello-world-system")
+    implicit val system: ActorSystem = ActorSystem("$name;format="Camel"$-system")
     implicit val materializer: ActorMaterializer = ActorMaterializer()
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
